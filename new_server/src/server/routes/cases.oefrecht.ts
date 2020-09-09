5afe797b932d8770ff86c26dbd8958442788c1eb
Paula Dettmann <paula.dettmann@gmail.com>
2020-09-09 18:01:08 +0200

    changed oe to ö in cases.oefrecht

--- new_server/src/server/routes/cases.oefrecht.ts
+++ new_server/src/server/routes/cases.oefrecht.ts
@@ -5,49 +5,49 @@ const router = Router()
 
 // gets oefrecht-staatsorganisationsrecht
 router.get('/oefrecht/staatsorganisationsrecht', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'Staatsorganisationsrecht' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'Staatsorganisationsrecht' })
 	res.render('oefrecht-staatsorganisationsrecht', { result })
 })
 
 // gets oefrecht-grundrechte
 router.get('/oefrecht/grundrechte', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'Grundrechte' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'Grundrechte' })
 	res.render('oefrecht-grundrechte', { result })
 })
 
 // gets oefrecht-europarecht
 router.get('/oefrecht/europarecht', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'Europarecht' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'Europarecht' })
 	res.render('oefrecht-europarecht', { result })
 })
 
 // gets oefrecht-verwaltungsrechtat
 router.get('/oefrecht/verwaltungsrechtat', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'Verwaltungsrecht AT' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'Verwaltungsrecht AT' })
 	res.render('oefrecht-verwaltungsrechtat', { result })
 })
 
 // gets oefrecht-verwaltungsprozessrecht
 router.get('/oefrecht/verwaltungsprozessrecht', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'Verwaltungsprozessrecht' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'Verwaltungsprozessrecht' })
 	res.render('oefrecht-verwaltungsprozessrecht', { result })
 })
 
 // gets oefrecht-por
 router.get('/oefrecht/por', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'POR' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'POR' })
 	res.render('oefrecht-por', { result })
 })
 
 // gets oefrecht-baurecht
 router.get('/oefrecht/baurecht', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'Baurecht' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'Baurecht' })
 	res.render('oefrecht-baurecht', { result })
 })
 
 // gets oefrecht-staatshaftungsrecht
 router.get('/oefrecht/staatshaftungsrecht', async (req: Request, res: Response, next: NextFunction) => {
-	let result = await Case.find({ categories: 'Oeffentliches Recht'}, { subcategories: 'Staatshaftungsrecht' })
+	let result = await Case.find({ categories: 'Öffentliches Recht'}, { subcategories: 'Staatshaftungsrecht' })
 	res.render('oefrecht-staatshaftungsrecht', { result })
 })
 
