0f168d7e8b6266be098ec447370b5a08bb37ad78
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-09-15 09:41:43 +0200

    removed not working script

--- new_server/src/client/public/scripts/scripts.js
+++ new_server/src/client/public/scripts/scripts.js
@@ -1,24 +1,23 @@
-function showSolution() {
+const showSolution = () => {
 	const x = document.getElementById('case-solution')
 	if (x.style.display === 'none') {
 		x.style.display = 'block'
 	} else {
 		x.style.display = 'none'
 	}
-};
+}
 
-var dropdown = document.getElementsByClassName("listLink");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-	dropdown[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var dropdownContent = this.nextElementSibling;
-		if (dropdownContent.style.display === "block") {
-			dropdownContent.style.display = "none";
-		} else {
-			dropdownContent.style.display = "block";
-		}
-	});
+const uploadTabs = (evt, cityName) => {
+	let i, tabcontent, tablinks
+	tabcontent = document.getElementsByClassName('tabcontent')
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none'
+	}
+	tablinks = document.getElementsByClassName('tablinks')
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(' active', '')
+	}
+	document.getElementById(cityName).style.display = 'block'
+	evt.currentTarget.className += ' active'
 }
 
