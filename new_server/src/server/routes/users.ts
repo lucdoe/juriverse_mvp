6ef7e37d00d7fdb35028e0ad4fe87a544ece80a9
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-09-16 17:45:16 +0200

    created profile page, needs styling now

--- new_server/src/server/routes/users.ts
+++ new_server/src/server/routes/users.ts
@@ -1,134 +1,37 @@
 import Cases from '../models/Case'
 import Users from '../models/User'
 import { Router, Response } from 'express'
-import { v4 as uuidv4 } from 'uuid'
+
 const router = Router()
 
-/* GET user profile. */
-router.get('/', (req: any, res: Response) => {
-	const data = req.user
-	res.render('profile', { data })
-})
-
-router.get('/:userid/cases', async (req: any, res: Response) => {
-	const id = req.params.userid
-	const allCases = await Cases.find({ $and: [{ 'meta.isPublished': true }, { 'meta.isDraft': false }, { 'author.authorId': id }] })
-	res.render('profile', { allCases })
-})
-
-
-// saving case content
-router.post('/:userid/cases', (req: any, res) => {
-	const user_id = req.params.userid
-
-	let { case_id, uploadTitle, sachverhalt, aufgabe, musterloesung, _public, draft } = req.body
-
-	if (!case_id) {
-		case_id = uuidv4()
-
-		const data = {
-			case_id,
-			author: {
-				author_id: user_id,
-				picture: req.user.picture,
-				name: req.user.nickname,
-				email: req.user.emails[0].value,
-				uni: '',
-			},
-			categories: [],
-			subcategories: [],
-			problems: [],
-			case: {
-				title: uploadTitle,
-				sachverhalt,
-				aufgabe,
-				musterloesung,
-				fussnoten: '',
-			},
-			meta: {
-				votes: 0,
-				favs: 0,
-				recommended: 0,
-				public: true,
-				draft: true,
-				uploadDate: '',
-			},
-			selfWriteConfirm: false,
+// get user profile
+router.get('/', async (req: any, res: Response) => {
+	const { user_id } = req.user
+	const user = await Users.findOne({ user_id })
+	await Cases.find({ 'author.authorId': user_id }, (err, allCases: any) => {
+		const result = {
+			user,
+			allCases,
+			authorName: req.user.displayName
 		}
-
-		Cases.create(data, async (err, data: any) => {
-			if (err) throw err
-			const { author, case_id } = data
-			const { author_id } = author
-			res.render('uploadStep3', { author_id, case_id })
-		})
-
-	} else if (case_id && sachverhalt || aufgabe || musterloesung) {
-
-		const data = {
-			case: {
-				title: uploadTitle,
-				sachverhalt,
-				aufgabe,
-				musterloesung,
-				fussnoten: '',
-			},
-		}
-
-		Cases.findOneAndUpdate({ case_id }, data, async (err, data: any) => {
-			if (err) throw err
-			console.log(data)
-			res.render('uploadStep3', { data })
-		})
-	}
-})
-
-router.post('/:userid/cases/modify', async (req: any, res) => {
-	const { draft, _public, case_id } = req.body
-	const user_id = req.params.userid
-	const data = {
-		meta: {
-			draft, public: _public
-		}
-	}
-	await Cases.updateOne({ case_id }, data, async (err, data) => {
-		await Users.findOne({ user_id }, async (err, data: any) => {
-			const draftCases = await Cases.find({ $and: [{ 'author.author_id': user_id }, { 'meta.draft': true }] }).exec()
-			const ownedCases = await Cases.find({ $and: [{ 'author.author_id': user_id }, { 'meta.draft': false }, { 'meta.public': true }] }).exec()
-			res.render('uploadStep1', { user_id, draftCases, ownedCases })
-		})
+		const data = req.user
+		res.render('profile', { result, data })
 	})
 })
 
-
-router.post('/:userid/cases/last', async (req: any, res) => {
-	const { case_id, categories, subcategories, problems } = req.body
-	const user_id = req.params.userid
-	const search = {
-		case_id
-	}
-	const data = {
-		categories: [categories],
-		subcategories: [subcategories],
-		problems: [problems],
-		meta: {
-			uploadDate: Date.now(),
-			public: true,
-			draft: false
-		},
-		selfWriteConfirm: true,
-	}
-	await Cases.updateOne(search, data)
-	res.redirect('http://localhost:3000/users/' + user_id + '/cases')
-})
-
-// NEW FROM HERE ON //
-
 // get one user
-router.get('/:id', async (req, res) => {
-	const { user_id } = req.params
-	const result = await Users.findOne({ user_id })
-	res.render('profile', { result })
+router.get('/:id', async (req: any, res) => {
+	const { id } = req.params
+	const data = req.user
+	const user = await Users.findOne({ userId: id })
+	await Cases.find({ 'author.authorId': id }, (err, allCases: any) => {
+		const result = {
+			user,
+			allCases,
+			authorName: allCases[0].author.name
+		}
+		res.render('profile', { result, data })
+	})
 })
 
 // get all cases for one user
