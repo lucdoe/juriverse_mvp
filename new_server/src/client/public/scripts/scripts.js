3e1052b22a7aae977baa40a3563044171d2ee6ca
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-09-17 14:24:55 +0200

    show solution button design changes

--- new_server/src/client/public/scripts/scripts.js
+++ new_server/src/client/public/scripts/scripts.js
@@ -1,12 +1,21 @@
 const showSolution = () => {
 	const x = document.getElementById('case-solution')
 	const y = document.getElementById('solution-box')
+	const z = document.getElementById('ssb1')
 	if (x.style.display === 'none') {
 		x.style.display = 'block'
 		y.style.display = 'block'
+		z.innerHTML = 'Musterlösung zuklappen'
+		z.style.color = '#cca768'
+		z.style.backgroundColor = 'white'
+		z.style.border = '1px solid #cca768'
 	} else {
 		x.style.display = 'none'
 		y.style.display = 'none'
+		z.innerHTML = 'Musterlösung anzeigen'
+		z.style.color = 'white'
+		z.style.backgroundColor = '#cca768'
+		z.style.border = 'none'
 	}
 }
 
