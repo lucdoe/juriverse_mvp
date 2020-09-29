2806061d709e88af5028099d08fc459c1185266e
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-09-29 13:52:57 +0200

    only show not deleted cases

--- new_server/src/server/routes/upload.ts
+++ new_server/src/server/routes/upload.ts
@@ -5,11 +5,12 @@ const router = Router()
 
 router.get('/', async (req: any, res: Response) => {
     const { user_id } = req.user
-    const publicCases = await Cases.find({ $and: [{ 'meta.isPublished': true }, { 'meta.isDraft': false }, { 'author.authorId': user_id }, { 'meta.isDeleted': false }] })
-    const draftCases = await Cases.find({ $and: [{ 'meta.isPublished': false }, { 'meta.isDraft': true }, { 'author.authorId': user_id }, { 'meta.isDeleted': false }] })
+    const publicCases = await Cases.find({ $and: [{ $or: [{ 'meta.isPublished': true }, { 'meta.isDraft': false }] }, { 'meta.isDeleted': false }, { 'author.authorId': user_id }] })
+    const draftCases = await Cases.find({ $and: [{ $or: [{ 'meta.isPublished': false }, { 'meta.isDraft': true }] }, { 'meta.isDeleted': false }, { 'author.authorId': user_id }] })
     const result = {
         publicCases,
-        draftCases
+        draftCases,
+        userId: user_id
     }
     res.render('yourCases', { result })
 })
