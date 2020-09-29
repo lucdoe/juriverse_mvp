24279cbf82a9b77d0939abe7d03dc25349e7f8b0
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-09-29 18:24:28 +0200

    added logger

--- new_server/app.ts
+++ new_server/app.ts
@@ -9,6 +9,7 @@ import bodyParser from 'body-parser'
 import session from 'express-session'
 import passport from 'passport'
 import Auth0Strategy from 'passport-auth0'
+import winston from 'winston'
 import { allowInsecurePrototypeAccess } from '@handlebars/allow-prototype-access'
 
 dotenv.config()
@@ -29,6 +30,33 @@ import { secured } from './src/server/middlewares/secured'
 import { userInViews } from './src/server/middlewares/userInViews'
 
 const app = express()
+
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.json(),
+	defaultMeta: { service: 'user-service' },
+	transports: [
+		//
+		// - Write all logs with level `error` and below to `error.log`
+		// - Write all logs with level `info` and below to `combined.log`
+		//
+		new winston.transports.File({ filename: 'error.log', level: 'error' }),
+		new winston.transports.File({ filename: 'combined.log' }),
+	],
+	exceptionHandlers: [
+		new winston.transports.File({ filename: 'exceptions.log' })
+	]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(new winston.transports.Console({
+		format: winston.format.simple(),
+	}));
+}
 
 // Configure Passport to use Auth0
 const strategy = new Auth0Strategy(
