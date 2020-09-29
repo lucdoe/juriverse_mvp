6db0652b1c35d7ee554bd600da858084d0b13b1f
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-09-29 20:45:51 +0200

    secure and https only cookies

--- new_server/app.ts
+++ new_server/app.ts
@@ -79,14 +79,15 @@ passport.use(strategy)
 // config express-session
 const sess = {
 	secret: process.env.session_secret,
-	cookie: {},
+	cookie: { maxAge: 3 * 3600000, secure: false, httpOnly: false, },
 	resave: false,
 	saveUninitialized: true,
 }
 
 if (process.env.NODE_ENV === 'production') {
 	// Use secure cookies in production (requires SSL/TLS)
-	sess.cookie = true
+	sess.cookie.secure = true
+	sess.cookie.httpOnly = true
 
 	// Uncomment the line below if your application is behind a proxy (like on Heroku)
 	// or if you're encountering the error message:
